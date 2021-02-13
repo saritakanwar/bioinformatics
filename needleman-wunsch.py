35c89addbef2f8601e4019654e6bbe11c0dc53e0
Sarita Kanwar <73707980+saritakanwar@users.noreply.github.com>
2021-02-13 16:56:25 +0530

    Create needleman-wunsch.py

---
+++ needleman-wunsch.py
@@ -0,0 +1,14 @@
+###----------------------------------Needlemqn-wunsch----------------------------------###
+
+# Importing Modules
+import numpy as np
+
+#Ask for sequence from the user
+#sequence1 = input("Enter or paste sequence1")
+#sequence2 = input("Enter or paste sequence2")
+sequence1 = "ATCGT"
+sequence2 = "ACGT"
+
+#Create Matrices
+main_matrix = np.zeros((len(sequence1)+1),len(sequence2)+1))
+
